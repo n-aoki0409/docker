5c922548987fde42648e0f8bf05484de844c5e27
n-aoki0409 <smijohn694fasfaf@gmail.com>
2022-05-15 20:18:02 +0900

    fix hive sql

--- apache-hadoop-ecosystem/script/hive/sample.sql
+++ apache-hadoop-ecosystem/script/hive/sample.sql
@@ -1,8 +1,13 @@
-create table emailid(custno int, email string) row format delimited fields terminated by ',' stored as textfile;
-create table txnrecords(txnno int, txndate string, custno int, amount double, category string, product string, city string, state string, spendby string) row format delimited fields terminated by ',' stored as textfile;
+create database retail;
+use retail;
 
-load data local inpath '/hive/script/email1.txt' into table emailid;
-load data local inpath '/hive/script/txns1.txt' into table txnrecords;
+create table emailid(custno int, email string) row format delimited fields terminated by ',' stored as textfile;
+create table txnrecords(txnno string, txndate string, custno int, amount double, product string, city string, state string, spendby string) partitioned by (category string) clustered by (state) INTO 10 BUCKETS row format delimited fields terminated by ',' stored as textfile;
+
+load data inpath '/user/hive/email1.txt' into table emailid;
+load data inpath '/user/hive/txns1.txt' into table txnrecords;
 
 select * from emailid;
 select * from txnrecords;
+
+-- insert into txnrecords partition(category='Road Sports') values('00000010','06-17-2011',4004799,252.46,'Skate Board','St. Petersburg','Florida', 'credit');
